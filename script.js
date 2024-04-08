b8198fba283e0437356c2d927b8201743855374d
Devilal panchal <devilalpanchal551@gmail.com>
2024-04-08 13:36:50 +0530

    add current location btn

--- script.js
+++ script.js
@@ -6,6 +6,33 @@ const region = document.querySelector('.region')
 const timezone = document.getElementById('timezone')
 const isp = document.getElementById('isp')
 
+// let input1    = document.querySelector('.input')
+      let btn1 = document.getElementById('UseyourLocation').addEventListener('click',()=>{
+async function fetchDetails() {
+        try {
+            let value
+          let url1 = "https://jsonip.com";
+          let response = await fetch(url1);
+          let data1 = await response.json();
+          let url2 = `https://ipapi.co/data1.ip/json`;
+          let response1 = await fetch(url2);
+          const ipAdress2 = data1.ip
+        //   let input1   = document.querySelector('.input').value = ipAdress2
+const input = document.getElementById('input').value = ipAdress2
+
+          console.log(ipAdress2);
+          let data = await response1.json();
+          trackLocation()
+
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      }
+
+      fetchDetails();
+
+      })
+
 
 function fetchData(url) {
 
@@ -40,6 +67,11 @@ function fetchData(url) {
 }
 
 btn.addEventListener('click', () => {
+ 
+  trackLocation()
+});
+
+function trackLocation() {
     const value = input.value.trim();
     if (value !== '') {
         const url = `https://geo.ipify.org/api/v1?apiKey=at_3KG6NmMqaezLciL4z0xSHfIKKrair&ipAddress=${value}`;
@@ -51,4 +83,4 @@ btn.addEventListener('click', () => {
         timezone.textContent = '';
         isp.textContent = '';
     }
-});
+}
